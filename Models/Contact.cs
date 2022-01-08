62e6ce263ac5445758cf8f3421623f2034b18e50
Kellor006 <richard.jbacsa@gmail.com>
2022-01-08 15:16:14 -0500

    Initial commit

---
+++ Models/Contact.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AddressBookMVC.Models
+{
+    public class Contact
+    {
+        //had to make some inputs Null in pgAdmin 4 - Edit -> Constraints not NULL
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public string? Address1 { get; set; }
+
+        public string? Address2 { get; set; }
+        public string? City { get; set; }
+        public string? Province { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        public string? PostalCode { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string? Phone { get; set; }
+
+        [NotMapped]
+        public DateTime? Created { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Image")]
+        [DataType(DataType.Upload)]
+        public IFormFile? ImageFile { get; set; }
+
+        public byte[]? ImageData { get; set; }
+
+        public string? ImageType { get; set; }
+
+        public int Id { get; set; }
+
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}";  } }
+
+
+    }
+}
