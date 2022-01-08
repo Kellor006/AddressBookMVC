6c8f8984214042f05e586c3bb09cb181018c04aa
Kellor006 <richard.jbacsa@gmail.com>
2022-01-08 16:44:41 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -25,6 +26,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
