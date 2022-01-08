08249cf4fdf5ad7ec1146c15e64c0873fc04d619
Kellor006 <richard.jbacsa@gmail.com>
2022-01-08 16:05:20 -0500

    Updated NAV

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using AddressBookMVC.Data;
 using AddressBookMVC.Services;
 using AddressBookMVC.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using System.Collections.Generic;
 
 
@@ -23,39 +24,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-
-var serviceProvider = builder.Services.BuildServiceProvider();
-try
-{
-    var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate();
-}
-catch
-{
-}
-
-var defaultConnectionString = string.Empty;
-
-if (builder.Environment.EnvironmentName == "Development")
-{
-    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-}
-else
-{
-    // Use connection string provided at runtime by Heroku.
-    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
-    var userPassSide = connectionUrl.Split("@")[0];
-    var hostSide = connectionUrl.Split("@")[1];
-
-    var user = userPassSide.Split(":")[0];
-    var password = userPassSide.Split(":")[1];
-    var host = hostSide.Split("/")[0];
-    var database = hostSide.Split("/")[1].Split("?")[0];
-
-    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
 }
 
 app.UseHttpsRedirection();
